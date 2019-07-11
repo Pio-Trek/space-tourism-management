0a871db77eb58055be4ed37f3bfe144fc8b93338
Piotr Przechodzki <pprzechodzki@gmail.com>
2019-07-11 13:00:50 +0200

    Added constants with URL for API mapping

---
+++ rest-api/src/main/java/eu/treative/spacetourism/utils/URLContants.java
@@ -0,0 +1,9 @@
+package com.eversis.datahub.utils;
+
+public class URLContants {
+
+    public static final String PATH = "http://localhost:";
+    public static final String API_MISSION = "/api/mission";
+    public static final String API_PRODUCT = "/api/product";
+
+}
