a7120a69a74434a4bcf9c82e8dbe969859e289a6
Piotr Przechodzki <pprzechodzki@gmail.com>
2019-07-11 16:57:44 +0200

    Created simple-sample DB initializer

--- rest-api/src/test/java/eu/treative/spacetourism/SpaceTourismApplicationTests.java
+++ rest-api/src/test/java/eu/treative/spacetourism/SpaceTourismApplicationTests.java
@@ -1,16 +1,16 @@
 package eu.treative.spacetourism;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class SpaceTourismApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
