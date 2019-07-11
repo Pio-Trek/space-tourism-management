40801a817b83a0f8e2b926b1b87bbf8dd0f9cc35
Piotr Przechodzki <pprzechodzki@gmail.com>
2019-07-11 11:06:52 +0200

    Rest API init commit

---
+++ rest-api/src/main/java/eu/treative/spacetourism/SpaceTourismApplication.java
@@ -0,0 +1,13 @@
+package eu.treative.spacetourism;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceTourismApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceTourismApplication.class, args);
+	}
+
+}
