6676ea8e555a0d05f2680eb1c0c5bbefcda9c9a4
Piotr Przechodzki <pprzechodzki@gmail.com>
2019-07-15 11:57:16 +0200

    Created empty flight and tourist index.html file

--- web-app/src/main/java/eu/treative/spacetourism/controller/FlightController.java
+++ web-app/src/main/java/eu/treative/spacetourism/controller/FlightController.java
@@ -2,9 +2,16 @@ package eu.treative.spacetourism.controller;
 
 import eu.treative.spacetourism.utils.URLContants;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping(URLContants.WEB_FLIGHT)
 public class FlightController {
+
+    @GetMapping
+    public String flightsList(Model model) {
+        return "/flight/index";
+    }
 }
--- web-app/src/main/java/eu/treative/spacetourism/controller/TouristController.java
+++ web-app/src/main/java/eu/treative/spacetourism/controller/TouristController.java
@@ -6,16 +6,13 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Date;
-
 @Controller
 @RequestMapping(URLContants.WEB_TOURIST)
 public class TouristController {
 
     @GetMapping
-    public String demo(Model model) {
-        model.addAttribute("date", new Date());
-        return "flight/demo";
+    public String touristsList(Model model) {
+        return "/tourist/index";
     }
 
 }
