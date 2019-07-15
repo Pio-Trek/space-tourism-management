71be9a87e83579f88aa3e29456ad0f4481f27258
Piotr Przechodzki <pprzechodzki@gmail.com>
2019-07-15 12:23:45 +0200

    Created data models

---
+++ web-app/src/main/java/eu/treative/spacetourism/model/Flight.java
@@ -0,0 +1,41 @@
+package eu.treative.spacetourism.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.FutureOrPresent;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Flight {
+
+    private Long id;
+
+    @NotNull
+    @FutureOrPresent
+    private LocalDateTime departure;
+
+    @NotNull
+    @FutureOrPresent
+    private LocalDateTime arrival;
+
+    @NotNull
+    @Min(value = 1, message = "The number of seats must be positive")
+    private Integer numberOfSeats;
+
+    @NotNull
+    @Min(value = 1, message = "The ticket price must be positive")
+    private Double ticketPrice;
+
+    private Set<Tourist> tourists = new HashSet<>();
+
+}
---
+++ web-app/src/main/java/eu/treative/spacetourism/model/Tourist.java
@@ -0,0 +1,42 @@
+package eu.treative.spacetourism.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Tourist {
+
+    private Long id;
+
+    @NotBlank
+    private String firstName;
+
+    @NotBlank
+    private String lastName;
+
+    private Gender gender;
+
+    @NotBlank
+    private String country;
+
+    private String remarks;
+
+    @NotNull
+    @PastOrPresent
+    private LocalDate dob;
+
+    private Set<Flight> flights = new HashSet<>();
+
+}
